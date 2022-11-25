e853f0389002b74f14147813fcc9351492f64eca
Manuelwils <manuelgwilz@gmail.com>
2022-11-25 05:18:16 -0300

    final commit

--- lib/utils/widget_binding_manager.dart
+++ lib/utils/widget_binding_manager.dart
@@ -8,36 +8,32 @@ class WidgetBindingManager extends StatefulWidget {
   State<WidgetBindingManager> createState() => _WidgetBindingManagerState();
 }
 
-class _WidgetBindingManagerState extends State<WidgetBindingManager> with WidgetsBindingObserver {
+class _WidgetBindingManagerState extends State<WidgetBindingManager>
+    with WidgetsBindingObserver {
   @override
   Widget build(BuildContext context) {
-    print('build running here');
     return widget.child!;
   }
 
   @override
   void initState() {
-    print('init running here');
-    WidgetsBinding.instance.addObserver(this);
+    WidgetsBinding.instance!.addObserver(this);
     super.initState();
   }
 
   @override
   void didUpdateWidget(covariant WidgetBindingManager oldWidget) {
-    print('old widget $oldWidget');
     super.didUpdateWidget(oldWidget);
   }
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
-    print("\n\n\n\t current state is: $state \n\n\n\t");
     super.didChangeAppLifecycleState(state);
   }
 
   @override
   void dispose() {
-    print('dispose running');
-    WidgetsBinding.instance.removeObserver(this);
+    WidgetsBinding.instance!.removeObserver(this);
     super.dispose();
   }
 }
